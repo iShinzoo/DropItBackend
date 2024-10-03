e4b5a751b79bcbed5526c644131077f73aa76edf
krishna thakur <115836360+krsnaimp@users.noreply.github.com>
2024-10-04 01:47:04 +0530

    First commit

---
+++ src/main/java/com/example/DropIT/DropItApplication.java
@@ -0,0 +1,13 @@
+package com.example.DropIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DropItApplication.class, args);
+	}
+
+}
